7ddcb7b94b61198dbcf3f37775d18daec4231ffb
manuj <manujdixit04@gmail.com>
2023-12-03 21:15:07 +0530

    second

--- src/pages/ShowBook.jsx
+++ src/pages/ShowBook.jsx
@@ -10,7 +10,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`https://books-b-d.onrender.com/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
